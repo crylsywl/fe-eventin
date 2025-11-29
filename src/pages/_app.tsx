03ade12a7c854a7df5227ef7ea83da811e148235
crylsywl <crylsyawal123@gmail,com>
2025-11-29 12:56:27 +0700

    Production

---
+++ src/pages/_app.tsx
@@ -0,0 +1,43 @@
+import AppShell from "@/components/commons/AppShell";
+import { ToasterProvider } from "@/contexts/ToasterContext";
+import { onErrorHander } from "@/libs/axios/reponseHandler";
+import "@/styles/globals.css";
+import { HeroUIProvider } from "@heroui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      throwOnError(error) {
+        onErrorHander(error);
+        return false;
+      },
+    },
+    mutations: {
+      onError: onErrorHander,
+    },
+  },
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <HeroUIProvider>
+          <ToasterProvider>
+            <AppShell>
+              <Component {...pageProps} />
+            </AppShell>
+          </ToasterProvider>
+        </HeroUIProvider>
+      </QueryClientProvider>
+    </SessionProvider>
+  );
+}
